3e9bd38cc3fb7dc5ee7fe1292c31758ad3834f97
DanielSrb <srb.dan@gmail.com>
2017-05-12 00:24:27 +0200

    example 2 is better now

--- day13-automation-and-preprocessors/example-02/gulpfile.js
+++ day13-automation-and-preprocessors/example-02/gulpfile.js
@@ -6,22 +6,21 @@ var clean = require('gulp-clean');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
-var exec = require('child_process').exec;
 
-// deletes css files
+// a task to delete all css files in main folder
 gulp.task('cssclean', function(){
 	return gulp.src('*.css', { read: false }).pipe(clean());
 });
 
-// CSS compilation + deletes css files first with previously defined task
+// CSS compilation (also deletes css files first using previously defined task)
 gulp.task('csscompile', ['cssclean'], function(){
 	return gulp
 		.src('index.scss') // this is the source of for compilation
-		.pipe(sass().on('error', sass.logError)) // tell us about a problem
+		.pipe(sass().on('error', sass.logError)) // compile sass to css and also tell us about a problem if happens
 		.pipe(sourcemaps.init()) // creates a sourcemap
-		.pipe(sourcemaps.write('.'))
-		.pipe(gulp.dest('./')) // destination of the result
-		.pipe(browserSync.stream()); // tell browsersync to reload CSS (injects)
+		.pipe(sourcemaps.write('.')) // writes the sourcemap
+		.pipe(gulp.dest('./')) // destination of the resulting css
+		.pipe(browserSync.stream()); // tell browsersync to reload CSS (injects compiled CSS)
 });
 
 gulp.task('develop', ['csscompile'], function(){
@@ -30,9 +29,9 @@ gulp.task('develop', ['csscompile'], function(){
 		server: {
 			baseDir: './' // serve from folder that this file is located
 		},
-		files: ['*.css', '*.html'] // watch for changes all files named anything.css and anything.html in the same folder gulpfile.js is located
+		files: ['*.html'] // watch for changes all files named anything.html in the same folder gulpfile.js is located
 	});
 	gulp.watch('*.scss', ['csscompile']); // watch for changes in scss
 });
 
-gulp.task('default', ['develop']); // set develop as a default task
+gulp.task('default', ['develop']); // set develop as a default task (gulp runs this when you don't specify a task)
